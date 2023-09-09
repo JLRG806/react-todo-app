759a82f2ca6c39fd3d1e5a10358443b2dbd88fef
José Luis Rodríguez <jose.rodriguez2@alumnos.uneatlantico.es>
2023-09-09 13:04:53 +0200

    feature: Input changes in modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,12 +9,15 @@ export function Modal({ children, status, onCloseModal }) {
                 <div className="modal-body">
                     <div className="hidden modal">
                         <label htmlFor="taskName" className="mb-4">
-                            <input className="appearance-none block w-[250px] bg-gray-200 text-gray-700 border rounded-full py-2 px-4 mx-4 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Task Name..." name="taskName" id="taskName" />
+                            <input className="appearance-none block w-[250px] bg-gray-200 text-gray-700 border rounded-full py-2 px-4 mx-4 focus:outline-none focus:bg-white" type="text" placeholder="Task Name..." name="taskName" id="taskName" />
                         </label>
 
-                        <label htmlFor="">
-                            <textarea className="appearance-none block w-[250px] bg-gray-200 text-gray-700 border rounded py-2 px-4 mx-4 leading-tight focus:outline-none focus:bg-white" name="" id="" rows="5"></textarea>
+                        <label htmlFor="" className="mb-3">
+                            <input className="appearance-none block w-[250px] bg-gray-200 text-gray-700 border rounded-full py-2 px-4 mx-4 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Category..." name="taskName" id="taskName" />
+                        </label>
 
+                        <label htmlFor="notes">
+                            <textarea id="notes" name="notes" rows="5" className="appearance-none block w-[250px] bg-gray-200 text-gray-700 border rounded py-2 px-4 mx-4 leading-tight focus:outline-none focus:bg-white" placeholder="Notes..."></textarea>
                         </label>
 
                         <div className="my-4">
