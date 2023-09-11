9dec7741b1148581977f1df9b45865aac90982e0
José Luis Rodríguez <jose.rodriguez2@alumnos.uneatlantico.es>
2023-09-11 22:44:43 +0200

    fix: Refactoring, typo errors.
    - Removed temporary code.
    - Import and rename to right name.
    - Set Modal status in button.
    - Some TODO comments, for future refactoring.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,43 @@
-import { Task } from "./../Task.js"
+import { ServiceTask } from "../service/serviceTask"
 import { Table } from "./../components/Table"
 import { Modal } from "./../components/Modal.jsx"
 import { useState } from "react"
 
 export function Home() {
-    const newTask = new Task()
 
-    const [taskList, setTaskList] = useState(newTask.getTasks())
+    //TODO: Refactor the initial state of task list using useEffect() hook, reason asynchronous.
+    const [taskList, setTaskList] = useState(ServiceTask.getTasks())
 
     const [modalStatus, setModalStatus] = useState(false)
-
-    function createNewTaskEvent(e) {
-
-        newTask.createNewTask({
-            taskName: "Prueba Mi loco - " + Math.floor((Math.random() * 100)),
-            lastChange: "Ayer Pa!!!",
-            category: "Trabajo"
-        })
-
-        setTaskList(newTask.getTasks())
-    }
 
     function taskHandler(id, action) {
 
         if (action === "edit") {
-            newTask.editTask(id)
+            ServiceTask.editTask(id)
         }
 
         if (action === "delete") {
-            newTask.deleteTaskById(id)
+            ServiceTask.deleteTask(id)
         }
 
-        setTaskList(newTask.getTasks())
+        setTaskList(ServiceTask.getTasks())
     }
 
+
+    //TODO:Refactor search with useEffect(), reason asynchronous
     function onSearch(e) {
         const searchString = e.target.value
         setTaskList(newTask.searchTasksByTaskName(searchString))
     }
 
-    function openModalEvent(e) {
-        
-    }
-
     const onCloseModal = () => {
         setModalStatus(status => status = !status)
+        setTaskList(ServiceTask.getTasks())
     }
 
     return (
         <>
             <div className="container-none">
-
-
                 <div className="container-none shadow-xl h-20 bg-aqua flex items-center justify-center">
                     <div className="text-3xl">
                         <h1>React TODO App</h1>
@@ -61,7 +47,7 @@ export function Home() {
                 <div className="w-[800px] h-[600px] my-5 mx-auto shadow-2xl rounded-lg">
                     <div className="flex flex-row justify-center items-center py-10">
                         <Modal status={modalStatus} onCloseModal={onCloseModal}>
-                            <button onClick={()=> setModalStatus(status => status = !status)} value={true} className="px-4 py-2 mx-4 font-semibold text-sm bg-aqua rounded-full shadow-sm">New Task</button>
+                            <button onClick={() => setModalStatus(status => status = !status)} className="px-4 py-2 mx-4 font-semibold text-sm bg-aqua rounded-full shadow-sm">New Task</button>
                         </Modal>
                         <br></br>
                         <input onChange={onSearch} className="appearance-none block w-[250px] bg-gray-200 text-gray-700 border rounded-full py-2 px-4 mx-4 leading-tight focus:outline-none focus:bg-white" id="search" type="text" placeholder="Search..."></input>
